8f3127be1f8f5d087b9bb589291ce5b5e35ef623
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2021-12-15 22:11:18 +0200

    Added smooth gradient

--- sunrise/sunrise.ino
+++ sunrise/sunrise.ino
@@ -52,30 +52,29 @@ struct Time {
   int seconds;
 };
 
-const Time DEFAULT_ALARM{22, 59, 030};
+const Time DEFAULT_ALARM{23, 22, 30};
 const int ALARM_START_THRESHOLD_SECONDS = 2;
 
-const int GRADIENT_LENGTH = 10;
+const int GRADIENT_LENGTH = 9;
 const Color GRADIENT_SEQUENCE[] = {
   {0, 0, 0},
   {0, 0, 10},
   {100, 0, 0},
   {200, 0, 0},
-  {255, 0, 0},
-  {255, 50, 50},
-  {255, 100, 100},
-  {255, 150, 150},
-  {255, 200, 200},
-  {255, 255, 255}
+  {200, 10, 10},
+  {200, 30, 30},
+  {200, 70, 70},
+  {200, 100, 100},
+  {200, 200, 200}
 };
 
 const int DELAY_LENGTH = 10;
 
-const int GRADIENT_DURATION_SECONDS = 5;
+const int GRADIENT_DURATION_SECONDS = 20;
 const int GRADIENT_STEP_DURATION = GRADIENT_DURATION_SECONDS * 1000 / GRADIENT_LENGTH;
 const int GRADIENT_STEP_ITERATIONS = GRADIENT_STEP_DURATION / DELAY_LENGTH;
 
-const int LCD_UPDATE_DELAY_STEPS = 50;
+const int LCD_UPDATE_DELAY_STEPS = 20;
 
 
 
@@ -85,9 +84,9 @@ class LightModel {
 
   bool started;
   bool finished;
-  int step;
-  int iteration;
   Color color;
+  unsigned long startTime;
+  double progress;
 
   public:
 
@@ -110,48 +109,68 @@ class LightModel {
   void reset() {
     started = false;
     finished = false;
-    step = 0;
-    iteration = 0;
+    progress = 0;
     color = GRADIENT_SEQUENCE[0];
   }
 
   void start() {
     reset();
     started = true;
+    this->startTime = millis();
   }
 
-  void update() {
+  void update(int gradientDuration) {
     if (!started || finished) {
       return;
     }
 
-    iteration++;
-    if (iteration == GRADIENT_STEP_ITERATIONS) {
-      step++;
-      iteration = 0;
-    }
-    if (step == GRADIENT_LENGTH - 1) {
+    unsigned long currTime = millis();
+    progress = (currTime - startTime) / (gradientDuration * 1000.);
+
+    if (progress >= 1) {
+      progress = 1;
       finished = true;
-      color = GRADIENT_SEQUENCE[step];
+      color = GRADIENT_SEQUENCE[GRADIENT_LENGTH - 1];
       return;
     }
+    
+    int step = floor((GRADIENT_LENGTH - 1) * progress);
+    double stepPart = 1. / GRADIENT_LENGTH;
+    double stepStart = step * stepPart;
+    double stepEnd = stepStart + stepPart;
+    double iterationProgress = (progress - stepStart) / (stepEnd - stepStart);
+    
 
     const Color currStepColor = GRADIENT_SEQUENCE[step];
     const Color nextStepColor = GRADIENT_SEQUENCE[step + 1];
-    const int newRed = currStepColor.red + (nextStepColor.red - currStepColor.red) * 1. * iteration / GRADIENT_STEP_ITERATIONS;
-    const int newGreen = currStepColor.green + (nextStepColor.green - currStepColor.green) * 1. * iteration / GRADIENT_STEP_ITERATIONS;
-    const int newBlue = currStepColor.blue + (nextStepColor.blue - currStepColor.blue) * 1. * iteration / GRADIENT_STEP_ITERATIONS;
+    const int newRed = filterColor(currStepColor.red + (nextStepColor.red - currStepColor.red) * iterationProgress);
+    const int newGreen = filterColor(currStepColor.green + (nextStepColor.green - currStepColor.green) * iterationProgress);
+    const int newBlue = filterColor(currStepColor.blue + (nextStepColor.blue - currStepColor.blue) * iterationProgress);
     color = {newRed, newGreen, newBlue};
+  }
+
+  int filterColor(int value) {
+    if (value < 0) {
+      return 0;
+    }
+    if (value > 255) {
+      return 255;
+    }
+    return value;
+  }
+
+  double getProgress() {
+    return progress;
   }
 
   String getStatus() {
     if (finished) {
-      return "++";
+      return "+";
     }
     if (started) {
-      return "--";
+      return "-";
     }
-    return "..";
+    return ".";
   }
 };
 
@@ -176,11 +195,15 @@ class ClockModel {
 
   void setup() {
     rtc.begin();
-//  rtc.settime(0, 45, 22, 13, 11, 21, 7); 
+    rtc.settime(10, 22, 23, 13, 11, 21, 7); 
   }
 
   String getCurrentTime() {
     return rtc.gettime("H:i:s");
+  }
+
+  unsigned long getUnix() {
+    return rtc.Unix;
   }
 
   bool shouldGradientStart(const Time &alarm, int gradientDuration) {
@@ -270,29 +293,31 @@ class LcdView {
 
   int step;
 
-  void printColor(const Color &color, const String &status) {
+  void printProgress(const String &status, const double progress) {
       lcd.setCursor(0, 0);
       lcd.print("                    ");
       lcd.setCursor(0, 0);
-      lcd.print(status);
-      lcd.print(color.red);
-      lcd.print(",");
-      lcd.print(color.green);
-      lcd.print(",");
-      lcd.print(color.blue);
+      int progressDec = progress * 100;
+      lcd.print(status + " " + String(progressDec) + "%");
   }
 
   String padNumber(int a) {
     return a < 10 ? String("0") + String(a) : String(a);
   }
 
-  void printTime(const String &currentTime, const Time &alarmTime) {
+  void printTime(const String &currentTime) {
+      lcd.setCursor(0, 1);
+      lcd.print("CLOCK:    " + currentTime);
+  }
+
+  void printAlarm(const Time &alarmTime) {
       lcd.setCursor(0, 2);
-      lcd.print("CLOCK: ");
-      lcd.print(currentTime);
+      lcd.print("ALARM:    " + padNumber(alarmTime.hours) + ":" + padNumber(alarmTime.minutes) + ":" + padNumber(alarmTime.seconds));
+  }
+
+  void printDuration(const int duration) {
       lcd.setCursor(0, 3);
-      lcd.print("ALARM: ");
-      lcd.print(padNumber(alarmTime.hours) + ":" + padNumber(alarmTime.minutes) + ":" + padNumber(alarmTime.seconds));
+      lcd.print("DURATION: " + String(duration) + "s");
   }
 
   public:
@@ -302,15 +327,18 @@ class LcdView {
   }
   
   void setup(){
-    lcd.init();                     
+    lcd.init();   
+    lcd.begin(20, 4);                  
     lcd.backlight();
   }
 
-  void update(const Color &color, const String &status, const String &currentTime, const Time &alarmTime) {
+  void update(const String &status, const double progress, const String &currentTime, const Time &alarmTime, const int gradientDuration) {
     step++;
     if (step == LCD_UPDATE_DELAY_STEPS) {
-      printColor(color, status);
-      printTime(currentTime, alarmTime);
+      printProgress(status, progress);
+      printDuration(gradientDuration);
+      printAlarm(alarmTime);
+      printTime(currentTime);
       step = 0;
     }
   }
@@ -352,15 +380,6 @@ class Controller {
   }
 
   void execute() {
-//    if (lightModel.isFinished()) {
-//      lightModel.reset();
-//    } else 
-//    if (!lightModel.isStarted()) {
-//      lightModel.start();
-//    } else {
-//      lightModel.update();
-//    }
-
     unsigned long signal = irModel.get();
     if (signal == SIGNAL_PLAY_PAUSE) {
       if (lightModel.isStarted()) {
@@ -376,12 +395,12 @@ class Controller {
           lightModel.start();
         }
       } else {
-        lightModel.update();
+        lightModel.update(settingsModel.getGradientDuration());
       }
     }
     
     lightView.update(lightModel.getColor());
-    lcdView.update(lightModel.getColor(), lightModel.getStatus(), clockModel.getCurrentTime(), settingsModel.getAlarm());
+    lcdView.update(lightModel.getStatus(), lightModel.getProgress(), clockModel.getCurrentTime(), settingsModel.getAlarm(), settingsModel.getGradientDuration());
   }
 };
 
